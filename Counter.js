dbad4e4166850c24c548ee829b0966482272e88c
Osman Büyükçerçi <121210041+OsmanBuyukcerci@users.noreply.github.com>
2023-02-08 16:25:23 +0000

    ver 0.11

--- Counter.js
+++ Counter.js
@@ -1,9 +1,10 @@
 
 var counter = 0;
+var saveCounter = 0;
 
 if (document.getElementById("counterSetting"))
 
-document.getElementById("rmvBut").onclick = function remove() {
+document.getElementById("lwrBut").onclick = function remove() {
     document.getElementById("counter").innerHTML = counter - parseFloat(document.getElementById("counterSetting").value);
     counter = counter - parseFloat(document.getElementById("counterSetting").value);
 }
@@ -19,5 +20,29 @@ document.getElementById("resBut").onclick = function res() {
 }
 
 document.getElementById("sveBut").onclick = function save() {
-    document.getElementById("sveOutput").innerHTML = "Saved Number: "+counter;
+    if (saveCounter == 0)
+    {
+        document.getElementById("li1").innerHTML = counter;
+    }
+    else if (saveCounter == 1)
+    {
+        document.getElementById("li2").innerHTML = counter;
+    }
+    else if (saveCounter == 2)
+    {
+        document.getElementById("li3").innerHTML = counter;
+    }
+    else if (saveCounter == 3)
+    {
+        document.getElementById("li4").innerHTML = counter;
+    }
+    else
+    {
+        document.getElementById("li5").innerHTML = counter;
+    }
+    ++saveCounter;
+    if (saveCounter == 5)
+    {
+        saveCounter = 0;
+    }
 }
